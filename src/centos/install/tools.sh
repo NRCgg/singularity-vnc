40463e0e770eca97996bffb10230e3183e8f2da6
Chester Gingrich <chester.gingrich@nrc.gov>
2023-11-08 21:10:50 -0500

    Slight 'update' changes to get this to work on my system

--- src/centos/install/tools.sh
+++ src/centos/install/tools.sh
@@ -4,7 +4,8 @@ set -e
 
 echo "Install some common tools for further installation"
 yum -y install epel-release 
+# the metalink/mirrors seems to fail for me.  I just changed to baseurl
+sed -i "s^metalink=https://mirrors.fedoraproject.org/metalink?repo=epel-7&arch=$basearch^baseurl=http://download.fedoraproject.org/pub/epel/7/$basearch^" /etc/yum.repos.d/epel.repo
 yum -y update
-yum -y install vim sudo wget which net-tools bzip2 \
-    numpy #used for websockify/novnc
+yum -y install vim sudo wget which net-tools bzip2 numpy
 yum clean all
--- src/common/install/no_vnc.sh
+++ src/common/install/no_vnc.sh
@@ -5,8 +5,11 @@ set -u
 
 echo "Install noVNC - HTML5 based VNC viewer"
 mkdir -p $NO_VNC_DEFAULT_HOME/utils/websockify
-wget -qO- https://github.com/novnc/noVNC/archive/v1.0.0.tar.gz | tar xz --strip 1 -C $NO_VNC_DEFAULT_HOME
 # use older version of websockify to prevent hanging connections on offline containers, see https://github.com/ConSol/docker-headless-vnc-container/issues/50
-wget -qO- https://github.com/novnc/websockify/archive/v0.6.1.tar.gz | tar xz --strip 1 -C $NO_VNC_DEFAULT_HOME/utils/websockify
+wget --no-check-certificate -O- https://github.com/novnc/noVNC/archive/refs/tags/v1.0.0.tar.gz > noVNC.tgz
+tar xvf noVNC.tgz -C $NO_VNC_DEFAULT_HOME --strip 1
+# use older version of websockify to prevent hanging connections on offline containers, see https://github.com/ConSol/docker-headless-vnc-container/issues/50
+wget --no-check-certificate -O- https://github.com/novnc/websockify/archive/refs/tags/v0.6.1.tar.gz > websock.tgz
+tar xvf websock.tgz -C $NO_VNC_DEFAULT_HOME/utils/websockify --strip 1
 chmod +x -v $NO_VNC_DEFAULT_HOME/utils/*.sh
 ## create index.html to forward automatically to `vnc_lite.html`
